0f0194b3e9c3f9dc14208478cbbf475efe983c0c
giovannilampis <giolampis@hotmail.com>
2023-05-22 15:21:52 +0200

    add method to form

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<!-- Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+
+
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+
+
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+
+<?php
+
+if (isset($_GET))
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,27 +34,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <title>php-strong-password-generator</title>
 </head>
 
 <body>
 
-    <form action="index.php">
+    <div class="container">
+        <form action="index.php" class="my-4" method="GET">
 
-        <div class="row">
+            <div class="row">
 
-            <label for="lenght" class="col-8">Insert password length</label>
+                <label for="lenght" class="col-8">Insert password length</label>
 
-            <div class="col-4">
+                <div class="col-4">
 
-                <input type="number" name="length" id="length">
+                    <input type="number" name="length" id="length">
+
+                </div>
+
 
             </div>
 
+        </form>
+    </div>
 
-        </div>
-
-    </form>
 
 </body>
 
