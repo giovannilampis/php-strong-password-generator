8eec347540dbe6baa7403e31631fa76d07dbe9db
giovannilampis <giolampis@hotmail.com>
2023-05-22 15:05:05 +0200

    add form to index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+</head>
+
+<body>
+
+    <form action="index.php">
+
+        <div class="row">
+
+            <label for=""></label>
+
+            <div>
+
+                <input type="number">
+
+            </div>
+
+
+        </div>
+
+    </form>
+
+</body>
+
+</html>
